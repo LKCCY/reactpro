0f34523006852d818f3d6f320a99a34aa78c6244
LKCCY <18810316505@163.com>
2018-09-07 21:10:43 +0800

    images

--- water-full/server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const mainCtrl = require('./controller.js/mainCtrl.js')
-app.get('/workInfo',mainCtrl.getWorkInfo);
-app.post('/upWork',mainCtrl.upWork);
-app.listen(3000,function(){
-    console.log('3000端口已开')
-})
--- water-full/src/models/util/workUtil.js
+++
@@ -1,5 +0,0 @@
-function* getInfo(put){
-   const {work} = yield fetch('/api/workInfo').then(res=>res.json());
-   return work;
-}
-export const getinfo = getInfo;
